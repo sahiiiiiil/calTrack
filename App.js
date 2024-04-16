9c31e60473847b7f4e76bc4bcfb7b13c3cc91896
sahiiiiiil <103057807+sahiiiiiil@users.noreply.github.com>
2024-04-16 00:09:07 -0700

    css getting better, protein box still out of the normal

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ const CalorieTracker = () => {
   const [food, setFood] = useState('');
   const [calories, setCalories] = useState('');
   const [protein, setProtein] = useState('');
+  const [bodyWeight, setBodyWeight] = useState('');
+  const [goalWeight, setGoalWeight] = useState('');
   const [totalCalories, setTotalCalories] = useState(0);
-  const [goalProtein, setGoalProtein] = useState(0); // New state for goal protein
+  const [totalProtein, setTotalProtein] = useState(0);
   const [foodEntries, setFoodEntries] = useState([]);
 
   const handleFoodChange = (text) => {
@@ -21,20 +23,54 @@ const CalorieTracker = () => {
     setProtein(text);
   };
 
+  const handleBodyWeightChange = (text) => {
+    setBodyWeight(text);
+  };
+
+  const handleGoalWeightChange = (text) => {
+    setGoalWeight(text);
+  };
+
   const handleClearCalories = () => {
     setTotalCalories(0);
   };
 
+  const handleClearProtein = () => {
+    setTotalProtein(0);
+  };
+
+  const handleClearFoodEntries = () => {
+    setFoodEntries([]);
+  };
+
+  const calculateSuggestedProtein = () => {
+    if (bodyWeight && goalWeight) {
+      const currentBodyWeight = parseFloat(bodyWeight);
+      const targetBodyWeight = parseFloat(goalWeight);
+      let suggestedProtein;
+
+      if (targetBodyWeight < currentBodyWeight) {
+        suggestedProtein = currentBodyWeight * 0.7;
+      } else {
+        suggestedProtein = currentBodyWeight * 1.0;
+      }
+
+      return Math.round(suggestedProtein);
+    }
+    return 0;
+  };
+
   const handleAddCalories = () => {
     const enteredCalories = parseInt(calories, 10);
-    const enteredProtein = parseInt(protein, 10); // Parse protein input
-
+    const enteredProtein = parseInt(protein, 10);
     if (!isNaN(enteredCalories) && !isNaN(enteredProtein) && food.trim() !== '') {
       const newTotal = totalCalories + enteredCalories;
+      const newTotalProtein = totalProtein + enteredProtein;
       setTotalCalories(newTotal);
+      setTotalProtein(newTotalProtein);
       setFoodEntries((prevEntries) => [
         ...prevEntries,
-        { food: food.trim(), calories: enteredCalories, protein: enteredProtein }, // Include protein in the entry
+        { food: food.trim(), calories: enteredCalories, protein: enteredProtein },
       ]);
       setFood('');
       setCalories('');
@@ -42,43 +78,246 @@ const CalorieTracker = () => {
     }
   };
 
-  const handleClearFoodEntries = () => {
-    setFoodEntries([]);
-  };
-  
+  const suggestedProtein = calculateSuggestedProtein();
+
   return (
-    <View style={{ marginTop: 50, padding: 20 }}>
-      <Text>Food:</Text>
-      <TextInput
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        value={food}
-        onChangeText={handleFoodChange}
-      />
-
-      <Text>Calories:</Text>
-      <TextInput
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        value={calories}
-        onChangeText={handleCaloriesChange}
-        keyboardType="numeric"
-      />
-
-      <Button title="Add Calories" onPress={handleAddCalories} />
-
-      <View>
-        <Text>Total Calories: {totalCalories}</Text>
-        <Button title="Clear Calories" onPress={handleClearCalories} />
-      </View>
-
-      <View>
-        <Text>Food Entries</Text>
-        <FlatList //use flatlist to render all items
+    <View style={{ marginTop: 100, padding: 20 }}>
+      <View
+        style={{
+          alignItems: 'center',
+          marginVertical: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: 'bold',
+          }}
+        >
+          Food Entries
+        </Text>
+        <FlatList
           data={foodEntries}
-          renderItem={({ item }) => ( //takes each item in data
-            <Text>{`${item.food}: ${item.calories} calories`}</Text>
+          renderItem={({ item }) => (
+            <Text
+              style={{
+                fontSize: 16,
+              }}
+            >{`${item.food}: ${item.calories} calories, ${item.protein}g protein`}</Text>
           )}
         />
-        <Button title="Clear Food Entries" onPress={handleClearFoodEntries} />
+        <Button
+          title="Clear Food Entries"
+          onPress={handleClearFoodEntries}
+          style={{
+            alignSelf: 'center',
+          }}
+        />
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginBottom: 10,
+        }}
+      >
+        <View
+          style={{
+            alignItems: 'center',
+            marginHorizontal: 10,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: 'bold',
+            }}
+          >
+            Body Weight (lbs):
+          </Text>
+          <TextInput
+            style={{
+              height: 40,
+              width: 150,
+              borderColor: 'gray',
+              borderWidth: 1,
+            }}
+            value={bodyWeight}
+            onChangeText={handleBodyWeightChange}
+            keyboardType="numeric"
+          />
+        </View>
+        <View
+          style={{
+            alignItems: 'center',
+            marginHorizontal: 10,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: 'bold',
+            }}
+          >
+            Goal Weight (lbs):
+          </Text>
+          <TextInput
+            style={{
+              height: 40,
+              width: 150,
+              borderColor: 'gray',
+              borderWidth: 1,
+            }}
+            value={goalWeight}
+            onChangeText={handleGoalWeightChange}
+            keyboardType="numeric"
+          />
+        </View>
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginBottom: 10,
+        }}
+      >
+        <View
+          style={{
+            alignItems: 'center',
+            marginHorizontal: 10,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: 'bold',
+            }}
+          >
+            Food:
+          </Text>
+          <TextInput
+            style={{
+              height: 40,
+              width: 150,
+              borderColor: 'gray',
+              borderWidth: 1,
+            }}
+            value={food}
+            onChangeText={handleFoodChange}
+          />
+        </View>
+        <View
+          style={{
+            alignItems: 'center',
+            marginHorizontal: 10,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: 'bold',
+            }}
+          >
+            Calories:
+          </Text>
+          <TextInput
+            style={{
+              height: 40,
+              width: 150,
+              borderColor: 'gray',
+              borderWidth: 1,
+            }}
+            value={calories}
+            onChangeText={handleCaloriesChange}
+            keyboardType="numeric"
+          />
+        </View>
+        <View
+          style={{
+            alignItems: 'center',
+            marginHorizontal: 10,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: 'bold',
+            }}
+          >
+            Protein (g):
+          </Text>
+          <TextInput
+            style={{
+              height: 40,
+              width: 150,
+              borderColor: 'gray',
+              borderWidth: 1,
+            }}
+            value={protein}
+            onChangeText={handleProteinChange}
+            keyboardType="numeric"
+          />
+        </View>
+      </View>
+
+      <Button
+        title="Add Calories & Protein"
+        onPress={handleAddCalories}
+        style={{
+          alignSelf: 'center',
+        }}
+      />
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginVertical: 10,
+        }}
+      >
+        <Text>Total Calories: {totalCalories}</Text>
+        <Button
+          title="Clear Calories"
+          onPress={handleClearCalories}
+          style={{
+            alignSelf: 'center',
+          }}
+        />
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginVertical: 10,
+        }}
+      >
+        <Text>Total Protein: {totalProtein}g</Text>
+        <View
+          style={{
+            marginLeft: 20,
+          }}
+        >
+          <Button
+            title="Clear Protein"
+            onPress={handleClearProtein}
+            style={{
+              alignSelf: 'center',
+            }}
+          />
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: 'bold',
+            }}
+          >
+            Suggested Protein (g): {suggestedProtein}
+          </Text>
+        </View>
       </View>
     </View>
   );
